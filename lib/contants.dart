346ff97c60834833b250a48ce2832bc4b3974a79
Safdar Nazir <safdarnazir14@gmail.com>
2020-12-03 20:09:49 +0500

    Another Push

---
+++ lib/contants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const activeCardColor = Color(0xff16193a);
+const inactiveCardColor = Color(0xff00002f);
+const bottomContainerButton = Colors.red;
+
+const labelTextStyle = TextStyle(
+  color: Color(0xffD3D3D3),
+  fontSize: 18,
+);
+
+const kNumberStyle = TextStyle(
+  color: Color(0xffD3D3D3),
+  fontSize: 80,
+);
